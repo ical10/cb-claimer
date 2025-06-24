0ebf8d285a37fdbd77827ca66c005a8c38a591f8
ical10 <mochhusnir@gmail.com>
2025-06-24 17:41:48 +0800

    feat: add reactive-dot and simple connect button

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from '@/components/ui/button'
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/ui/card'
+import ClaimCard from '@/components/ui/claim-card'
+import { config } from '../config'
+import { ChainProvider, ReactiveDotProvider } from '@reactive-dot/react'
+import { Suspense } from 'react'
+
 
 function App() {
-  const [isConnected, setIsConnected] = useState(false)
-
-  const handleClick = () => {
-    console.log("clicked");
-  }
-
   return (
-    <div className='flex flex-col justify-center h-screen'>
-      <Card className='w-full max-w-sm mx-auto'>
-        <CardHeader className='flex-col gap-2'>
-          <CardTitle className='font-unbounded text-2xl'>Child Bounty Claimer</CardTitle>
-          <CardDescription className='font-unbounded font-light text-gray-800'>Easily claim your child bounties</CardDescription>
-        </CardHeader>
-        <CardContent>
-          {
-            isConnected ? 
-          <p>My account: </p> : <Button>Connect</Button>
-          }
-        </CardContent>
-        <CardFooter className='flex-col gap-2'>
-          <p>this is the rewards</p>
-        </CardFooter>
-      </Card>
-    </div>
+    <>
+        <ReactiveDotProvider config={config}>
+          <ChainProvider chainId="polkadot">
+            <Suspense fallback={<div>Loading...</div>}>
+              <ClaimCard />
+            </Suspense>
+          </ChainProvider>
+        </ReactiveDotProvider>
+    </>
   )
 }
 
