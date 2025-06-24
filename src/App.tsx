c90af0aeb24b9c917b290ed867f3848fc76c8d39
ical10 <mochhusnir@gmail.com>
2025-06-24 14:07:25 +0800

    feat: create basic template

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from '@/components/ui/button'
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/ui/card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isConnected, setIsConnected] = useState(false)
+
+  const handleClick = () => {
+    console.log("clicked");
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='flex flex-col justify-center h-screen'>
+      <Card className='w-full max-w-sm mx-auto'>
+        <CardHeader className='flex-col gap-2'>
+          <CardTitle className='font-unbounded text-2xl'>Child Bounty Claimer</CardTitle>
+          <CardDescription className='font-unbounded font-light'>Easily claim your child bounties</CardDescription>
+        </CardHeader>
+        <CardContent>
+          {
+            isConnected ? 
+          <p>My account: </p> : <Button>Connect</Button>
+          }
+        </CardContent>
+        <CardFooter className='flex-col gap-2'>
+          <p>this is the rewards</p>
+        </CardFooter>
+      </Card>
+    </div>
   )
 }
 
